edfd533505d35f0463183a5b5bb4a618eaab6cba
ENTIT_Y <raphaelagyapong21@gmail.com>
2026-05-18 00:23:49 +0000

    Chat widget 11

--- server.js
+++ server.js
@@ -2082,8 +2082,7 @@ app.get('/forgot-password', (req, res) => res.sendFile(path.join(__dirname, 'pub
 app.get('/verify-email', (req, res) => res.sendFile(path.join(__dirname, 'public', 'verify-email.html')));
 app.get('/reset-password', (req, res) => res.sendFile(path.join(__dirname, 'public', 'reset-password.html')));
 app.get('/workspaces', (req, res) => serveWithConfig(res, 'workspaces.html'));
-app.get('/feed', (req, res) => res.sendFile(path.join(__dirname, 'public', 'feed.html')));
-app.get('/invite', (req, res) => res.sendFile(path.join(__dirname, 'public', 'invite.html')));
+app.get('/feed', (req, res) => serveWithConfig(res, 'feed.html'));app.get('/invite', (req, res) => res.sendFile(path.join(__dirname, 'public', 'invite.html')));
 app.get('/vdp', (req, res) => res.sendFile(path.join(__dirname, 'public', 'vdp.html')));
 app.get('/', (req, res) => {
   const token = req.cookies.token;
